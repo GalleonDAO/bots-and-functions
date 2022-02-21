820f77f9749a3d3e93e8591ce5fb34f3b5bbaf35
Andrew <andywilkinson1993@gmail.com>
2022-02-21 12:12:16 +0000

    production

--- netlify/functions/dbl-price-bot/utils.js
+++
@@ -1,1 +0,0 @@
-exports.numberWithCommas = (num) => num.toLocaleString()
---
+++ netlify/functions/dbl-price/dbl-price.js
@@ -0,0 +1,74 @@
+const { Client } = require('discord.js')
+const dotenv = require('dotenv')
+const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch')
+
+dotenv.config()
+
+let client = new Client()
+client.login(process.env.DISCORD_API_TOKEN)
+client.on('ready', () =>
+  console.log(`Bot successfully started as ${client.user.tag} 🤖`),
+)
+
+module.exports.handler = schedule('* * * * *', async (event) => {
+  const eventBody = JSON.parse(event.body)
+  console.log(`Next function run at ${eventBody.next_run}.`)
+
+  if (client === null || client === undefined) {
+    client = new Client()
+    client.login(process.env.DISCORD_API_TOKEN)
+    client.on('ready', () =>
+      console.log(`Bot successfully started as ${client.user.tag} 🤖`),
+    )
+  }
+
+  const data = await fetchData()
+
+  if (!data) return
+
+  const { price, symbol, circSupply } = data
+
+  console.log('Fetched: ' + symbol, price, circSupply)
+
+  client.guilds.cache.forEach(async (guild) => {
+    const botMember = guild.me
+    await botMember.setNickname(`${price ? numberWithCommas(price) : ''}`)
+  })
+
+  if (client.user) {
+    client.user.setActivity(
+      `${
+        circSupply
+          ? 'MC: $' + numberWithCommas(Math.round(price * circSupply))
+          : ''
+      }`,
+      { type: 'WATCHING' },
+    )
+  }
+
+  return {
+    statusCode: 200,
+  }
+})
+
+const fetchData = async () => {
+  try {
+    const tokenData = await (
+      await fetch(
+        `https://api.coingecko.com/api/v3/coins/${process.env.TOKEN_ID}`,
+      )
+    ).json()
+
+    const price = tokenData.market_data.current_price.usd
+    const symbol = tokenData.symbol.toUpperCase()
+    const circSupply = tokenData.market_data.circulating_supply
+
+    return { price, symbol, circSupply }
+  } catch (err) {
+    console.log(err)
+    return undefined
+  }
+}
+
+const numberWithCommas = (num) => num.toLocaleString()
